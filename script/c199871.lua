dfcdda1cf069ed41e99a5f2f2ff68a4553df3b7c
raiderpls <89932011+raiderpls@users.noreply.github.com>
2021-09-03 18:46:20 -0400

    Add files via upload

---
+++ script/c199871.lua
@@ -0,0 +1,109 @@
+-- Darklord Samyaza
+-- raider
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Can only Special once per turn
+	c:SetSPSummonOnce(id)
+
+    -- Banish to summon
+	local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_GRAVE)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetCondition(s.descon)
+    e1:SetCost(s.descost)
+    e1:SetTarget(s.destg)
+    e1:SetOperation(s.desop)
+    c:RegisterEffect(e1)
+
+	-- Darklord copy effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,id+1)
+	e2:SetCost(s.cpcost)
+	e2:SetTarget(s.cptg)
+	e2:SetOperation(s.cpop)
+	c:RegisterEffect(e2)
+end
+
+s.listed_series={0xef}
+
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_GRAVE,0,nil,0xef):GetClassCount(Card.GetCode) >= 2
+end
+
+function s.cfilter(c)
+    return c:IsSetCard(0xef) and c:IsAbleToRemoveAsCost()
+end
+
+function s.filter(c,e,tp)
+    return c:IsSetCard(0xef) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+end
+
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return aux.bfgcost(e,tp,eg,ep,ev,re,r,rp,0) and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+    g:AddCard(e:GetHandler())
+    Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
+end
+
+function s.cpcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+
+function s.cpfilter(c)
+	return c:IsSetCard(0xef) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToDeck() and c:CheckActivateEffect(false,true,false)~=nil
+end
+
+function s.cptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.cpfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.cpfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.cpfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+
+function s.cpop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not (tc and tc:IsRelateToEffect(e)) then return end
+	local te,ceg,cep,cev,cre,cr,crp=tc:CheckActivateEffect(false,true,true)
+	if not te then return end
+	local tg=te:GetTarget()
+	local op=te:GetOperation()
+	if tg then tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,1) end
+	Duel.BreakEffect()
+	tc:CreateEffectRelation(te)
+	Duel.BreakEffect()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	for etc in aux.Next(g) do
+		etc:CreateEffectRelation(te)
+	end
+	if op then op(te,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,1) end
+	tc:ReleaseEffectRelation(te)
+	for etc in aux.Next(g) do
+		etc:ReleaseEffectRelation(te)
+	end
+	Duel.BreakEffect()
+	Duel.SendtoDeck(te:GetHandler(),nil,2,REASON_EFFECT)
+end
