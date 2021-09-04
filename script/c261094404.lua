fd2a555d777f2683268525473b14e079a9fba6f0
raiderpls <89932011+raiderpls@users.noreply.github.com>
2021-09-03 20:36:28 -0400

    Add files via upload

---
+++ script/c261094404.lua
@@ -0,0 +1,73 @@
+-- Witchcrafter Roku
+local s, id = GetID()
+function s.initial_effect(c)
+  --special summon
+  local e1=Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_FIELD)
+  e1:SetCode(EFFECT_SPSUMMON_PROC)
+  e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+  e1:SetRange(LOCATION_HAND)
+  e1:SetCondition(s.spcon)
+  e1:SetTarget(s.sptg)
+  e1:SetOperation(s.spop)
+  c:RegisterEffect(e1)
+
+  local e1=Effect.CreateEffect(c)
+  e1:SetDescription(aux.Stringid(id,0)) -- Add Witchcrafter Spell/Trap.
+  e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+  e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+  e1:SetCode(EVENT_SUMMON_SUCCESS)
+  e1:SetCountLimit(1,id)
+  e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+  e1:SetTarget(s.thtg)
+  e1:SetOperation(s.thop)
+  c:RegisterEffect(e1)
+  local e2=e1:Clone()
+  e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+  c:RegisterEffect(e2)
+end
+s.listed_series={0x128}
+
+-- Special Summon
+function s.spfilter(c)
+	return c:IsType(TYPE_SPELL) and c:IsAbleToRemoveAsCost()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,tp)
+	return aux.SelectUnselectGroup(g,e,tp,1,1,nil,0) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,tp)
+	local rg=aux.SelectUnselectGroup(g,e,tp,1,1,nil,1,tp,HINTMSG_REMOVE,nil,nil,true)
+	if #rg>0 then
+		rg:KeepAlive()
+		e:SetLabelObject(rg)
+		return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local rg=e:GetLabelObject()
+	if not rg then return end
+	Duel.Remove(rg,POS_FACEUP,REASON_COST)
+	rg:DeleteGroup()
+end
+
+-- Add S/T
+function s.filter(c)
+	return c:IsSetCard(0x128) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
