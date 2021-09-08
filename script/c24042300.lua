c5d2de8564c4ee55b4203adf2e45c1d65ff335f0
raiderpls <89932011+raiderpls@users.noreply.github.com>
2021-09-07 20:41:02 -0400

    Add files via upload

---
+++ script/c24042300.lua
@@ -0,0 +1,91 @@
+-- Spellborn Star Child
+-- raider
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Xyz Summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_SPELLCASTER),8,2,s.ovfilter,aux.Stringid(id,0),99,s.xyzop)
+	c:EnableReviveLimit()
+
+	-- Original ATK/DEF
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(s.boostvalue)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	e2:SetValue(s.boostvalue)
+	c:RegisterEffect(e2)
+
+	-- Burn if card leaves field
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCost(s.cost)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3,false,REGISTER_FLAG_DETACH_XMAT)
+end
+
+
+function s.ovfilter(c,tp,lc)
+	local g=Duel.GetMatchingGroup(Card.IsType,c:GetControler(),LOCATION_GRAVE,0,nil,TYPE_SPELL)
+	local differentSpells=g:GetClassCount(Card.GetCode)
+
+	return c:IsFaceup() and c:IsRace(RACE_SPELLCASTER,lc,SUMMON_TYPE_XYZ,tp) and c:IsLevelBelow(8) and differentSpells >= 3
+end
+
+function s.xyzop(e,tp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	return true
+end
+
+function s.boostvalue(e,c)
+	local g=Duel.GetMatchingGroup(Card.IsType,c:GetControler(),LOCATION_GRAVE,0,nil,TYPE_SPELL)
+	local differentSpells=g:GetClassCount(Card.GetCode)
+
+	return differentSpells * 500
+end
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local e2=Effect.CreateEffect(tc)
+		e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+		e2:SetCode(EVENT_LEAVE_FIELD_P)
+		e2:SetOperation(s.regop)
+		e2:SetReset(RESET_EVENT+RESETS_REDIRECT+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2,true)
+	end
+end
+
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetOperation(s.damop)
+	e:GetHandler():RegisterEffect(e1,true)
+end
+
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Damage(tp,2000,REASON_EFFECT)
+	e:Reset()
+end
