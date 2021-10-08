6e060782a001754293b446d2a541662a01f3b3c9
raiderpls <89932011+raiderpls@users.noreply.github.com>
2021-10-08 14:49:51 -0400

    Add files via upload

---
+++ script/c94034895.lua
@@ -0,0 +1,85 @@
+-- Star Serpent
+-- raider
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Link Summon
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,s.matfilter,1,1)
+
+	-- Use set monsters
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetCode(EFFECT_EXTRA_MATERIAL)
+	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET|EFFECT_FLAG_CANNOT_DISABLE|EFFECT_FLAG_SET_AVAILABLE)
+	e0:SetTargetRange(1,0)
+	e0:SetValue(s.extraval)
+	c:RegisterEffect(e0)
+
+	-- Extra Set
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetOperation(s.sumop)
+	c:RegisterEffect(e1)
+
+	-- Flip Summon if sent to GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetTarget(s.fltg)
+	e3:SetOperation(s.flop)
+	c:RegisterEffect(e3)
+end
+
+function s.sumcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+
+function s.matfilter(c,lc,stype,tp)
+	return c:IsFacedown()
+end
+
+function s.extraval(chk,summon_type,e,...)
+	if chk==0 then
+		local tp,sc=...
+		if summon_type~=SUMMON_TYPE_LINK or sc~=e:GetHandler() then
+			return Group.CreateGroup()
+		else
+			return Duel.GetMatchingGroup(Card.IsFacedown,tp,LOCATION_MZONE,0,nil)
+		end
+	end
+end
+
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,id)~=0 then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e1:SetCode(EFFECT_EXTRA_SET_COUNT)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+end
+
+function s.flfilter(c)
+	return c:IsFacedown()
+end
+
+function s.fltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.flfilter,tp,LOCATION_MZONE,0,1,nil) end
+end
+
+function s.flop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.IsExistingMatchingCard(s.flfilter,tp,LOCATION_MZONE,0,1,nil) then
+		local g2=Duel.SelectMatchingCard(tp,s.flfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,exc)
+		if #g2>0 then
+			local tc2 = g2:GetFirst()
+			Duel.ChangePosition(tc2,POS_FACEUP_ATTACK)
+		end
+	end
+end
