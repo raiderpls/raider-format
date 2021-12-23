f0db8239c95e550d0163238ab49f9b931a9da424
raiderpls <89932011+raiderpls@users.noreply.github.com>
2021-12-23 18:20:25 -0500

    Add files via upload
    added script for Tunewing Caller

---
+++ script/c58922799.lua
@@ -0,0 +1,102 @@
+-- Tunewing Caller
+-- raider
+
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+
+	-- Banish from deck to search
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.rmtg)
+	e2:SetOperation(s.rmop)
+	c:RegisterEffect(e2)
+end
+
+s.listed_names={id}
+
+function s.spfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+end
+
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp = c:GetControler()
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,c)
+	return #g>0 and aux.SelectUnselectGroup(g,e,tp,1,1,nil,0) and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,c)
+	local rg=aux.SelectUnselectGroup(g,e,tp,1,1,nil,1,tp,HINTMSG_REMOVE,nil,nil,true)
+	if #rg>0 then
+		rg:KeepAlive()
+		e:SetLabelObject(rg)
+		return true
+	end
+	return false
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	-- Banish selected monster
+	local rg=e:GetLabelObject()
+	if not rg then return end
+	Duel.Remove(rg,POS_FACEUP,REASON_COST)
+	rg:DeleteGroup()
+
+	-- Banish it if it leaves the field
+	local c = e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(3300)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+	e1:SetValue(LOCATION_REMOVED)
+	e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+	c:RegisterEffect(e1,true)
+end
+
+function s.cfilter(c,tp)
+	return c:IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(s.dfilter,tp,LOCATION_DECK,0,1,nil,c:GetRace(),c:GetAttribute(),c:GetLevel())
+end
+
+function s.dfilter(c,rc,atr,lv)
+	return (not c:IsRace(rc))
+		and (not c:IsAttribute(atr))
+		and (c:HasLevel() and (c:GetLevel() - lv == 1 or c:GetLevel() - lv == -1))
+		and c:IsAbleToRemove()
+end
+
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE+CATEGORY_TOHAND+CATEGORY_SEARCH,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tp)
+	local c=g:GetFirst()
+
+	if Duel.Remove(g,POS_FACEUP,REASON_COST) ~= 0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g2=Duel.SelectMatchingCard(tp,s.dfilter,tp,LOCATION_DECK,0,1,1,nil,c:GetRace(),c:GetAttribute(),c:GetLevel())
+		local tg=g2:GetFirst()
+		if tg==nil then return end
+		Duel.SendtoHand(g2,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g2)
+	end
+end
